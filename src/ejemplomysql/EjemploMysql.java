66a16d689f8ed3b2401a0e75f8f0d0797f7cfaf0
ESTUDIANTE <ESTUDIANTE@10.220.7.157>
2023-09-05 14:54:11 -0500

    bueno

---
+++ src/ejemplomysql/EjemploMysql.java
@@ -0,0 +1,37 @@
+
+package ejemplomysql;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class EjemploMysql {
+    
+    private static java.sql.Connection con;
+    
+    public static String driver  = "com.mysql.jdbc.Driver";
+    public static String user = "root";
+    public static String pass = "";
+    public static String url = "jdbc:mysql://localhost:3306/dbtarde";
+    
+    public static void main(String[] args) throws SQLException{
+        
+        con = null;
+        try {
+
+            con =  DriverManager.getConnection(url, user, pass);
+            if (con != null) {
+                System.out.println("Conexión exitosa");
+            }
+        } catch (SQLException e) {
+            System.out.println("Conexión no exitosa");
+        }
+           
+        Operaciones operacion = new Operaciones();
+        
+        operacion.guardar(con);
+        
+    }
+    
+}
